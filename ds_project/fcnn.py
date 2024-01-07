e14c5dcda0d7d835048bc3bd1b654a93fcd224ca
Artem Dikov <35744124+ArtemD@users.noreply.github.com>
2024-01-07 15:06:21 +0300

    Fix flatten in separate file

--- ds_project/fcnn.py
+++ ds_project/fcnn.py
@@ -1,12 +1,10 @@
 from torch import nn
 
-from ds_project.flatten import Flatten
-
 
 def new_fcnn(size_h, size_w, dense_size, embedding_size, num_classes):
     model = nn.Sequential()
 
-    model.add_module("flatten", Flatten())
+    model.add_module("flatten", nn.Flatten(start_dim=1))
 
     model.add_module("dense1", nn.Linear(3 * size_h * size_w, dense_size))
     model.add_module("dense1_relu", nn.ReLU())
--- ds_project/flatten.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch import nn
-
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return torch.flatten(x, start_dim=1)
